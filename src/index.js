4d02c1dea3af64187470a082d57198eeeb838004
Vishnu Nair <vishnunair@vishnus-mbp.myfiosgateway.com>
2020-07-06 15:08:10 -0400

    many changes

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import resolvers from './resolvers';
 import mongoose from 'mongoose';
 
 const PORT = 3002;
-//
+// 
 const startServer = async () => {
     await mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true });
 
